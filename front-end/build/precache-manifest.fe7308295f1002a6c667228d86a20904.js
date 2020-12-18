15afd16469fe2c53bd4f36a81d8bfe4c582ede71
Dylan Cunningham <gdylanc@gmail.com>
2020-12-18 16:03:52 -0700

    adds front-end subfolders

---
+++ front-end/build/precache-manifest.fe7308295f1002a6c667228d86a20904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a991eaf9febeccd7a7fed2e11e60d3d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21a48796cd452e8dc4f",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "25a436ee25ecabd7ef82",
+    "url": "/static/css/main.603a9257.chunk.css"
+  },
+  {
+    "revision": "b21a48796cd452e8dc4f",
+    "url": "/static/js/2.79105a04.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/static/js/2.79105a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a436ee25ecabd7ef82",
+    "url": "/static/js/main.fcd890fb.chunk.js"
+  },
+  {
+    "revision": "4afe7fdc77e549553e29",
+    "url": "/static/js/runtime-main.442ecfea.js"
+  }
+]);
